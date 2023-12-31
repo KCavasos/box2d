51dfef3695b6085b30d3ea1b7c630916756b3dfe
KCavasos <kyleecavasos@gmail.com>
2023-12-31 14:53:15 -0800

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C"
+	staticruntime "Off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediates/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/box2d/**.h",
+		"src/**.cpp"
+		"src/**.h"
+	}
+
+	includedirs
+	{
+		"include/box2d",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
